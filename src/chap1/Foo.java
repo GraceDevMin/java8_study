4c9c9253399b0898b58a2a2c3a7279858ec426e5
민은혜 <eunhye.min@myvtw.co.kr>
2022-03-03 10:30:41 +0900

    lambda expression

--- src/chap1/Foo.java
+++ src/chap1/Foo.java
@@ -1,25 +1,45 @@
 package chap1;
 
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.Supplier;
+
 public class Foo {
 
     public static void main(String[] args) {
-        // 익명 내부 클래스 (anonymous inner class)
-//        RunSomething runSomething = new RunSomething() {
-//            @Override
-//            public void doIt() {
-//                System.out.println("Hello");
-//            }
-//        }
+    	Foo foo = new Foo();
+    	foo.run();
 
-        RunSomething runSomething = new RunSomething() {
-            final int baseNumber = 10; //함수 밖에 있는 값, 상태값을 가지고 있다. 의존한다. -> 퓨어한 함수X
-            //또는 외부의 있는 값을 변경하는 경우 -> 퓨어한 함수 X
-            @Override
-            public int doIt(int number) {
-                return number + baseNumber;
-            }
-        }
+    }
+    
+    private void run() {
+    	int baseNumber = 10; //effective final
+    	
+    	//로컬 클래스 (셰도잉 O)
+    	class LocalClass {
+    		void printBaseNumber() {
+    			int baseNumber = 11;
+    			System.out.println(baseNumber);
+    		}
+    	}
+    	
+    	//익명 클래스 (셰도잉 O)
+    	Consumer<Integer> integerConsumer = new Consumer<Integer>() {
 
-
+			@Override
+			public void accept(Integer baseNumber) {
+				System.out.println(baseNumber);
+				
+			}
+		};
+    	
+    	//람다 (셰도잉 X, 이 람다를 감싸고 있는 run과 같은 scope)
+		IntConsumer printInt = (i) -> {
+			System.out.println(i + baseNumber);
+		};
+		
+		printInt.accept(10);
+		
+//		baseNumber++; 
     }
 }
