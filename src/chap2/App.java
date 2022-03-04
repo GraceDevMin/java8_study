bbf577e011da6d5ec5e037dbce59fe0b4915a96b
민은혜 <olivgrece@gmail.com>
2022-03-04 14:56:36 +0900

    Java 8 API > default method and static method

--- src/chap2/App.java
+++ src/chap2/App.java
@@ -1,14 +1,40 @@
 package chap2;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
 
 	public static void main(String[] args) {
-		Foo foo = new DefaultFoo("keesun");
-		foo.printName();
-		foo.printNameUpperCase();
+		List<String> name = new ArrayList<>();
+		name.add("keesun");
+		name.add("whiteship");
+		name.add("toby");
+		name.add("foo");
 		
-		Foo.printAnyString();
+//		name.forEach(System.out::println);
 		
-		//git test
+		Spliterator<String> spliterator = name.spliterator();
+		Spliterator<String> spliterator1 = spliterator.trySplit();
+		while (spliterator.tryAdvance(System.out::println));
+		System.out.println("========================");	
+		while (spliterator1.tryAdvance(System.out::println));
+		
+		System.out.println("========================");
+		
+		long k = name.stream().map(String::toUpperCase)
+				.filter(s -> s.startsWith("K"))
+				.count();
+		
+		System.out.println(k);
+		
+		Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+		name.sort(compareToIgnoreCase.reversed());
+		
+		name.removeIf(s -> s.startsWith("k"));
+		name.forEach(System.out::println);
+		
 	}
 }
