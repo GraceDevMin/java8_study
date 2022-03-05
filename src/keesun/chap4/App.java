5eedbc43381a26762c8bf201a31f194a686e60fc
GraceDeveMin <olivgrece@gmail.com>
2022-03-05 12:34:04 +0900

    Optional API

--- src/keesun/chap4/App.java
+++ src/keesun/chap4/App.java
@@ -13,16 +13,39 @@ public class App {
 	public static void main(String[] args) {
 		List<OnlineClass> springClasses = new ArrayList<>();
 		springClasses.add(new OnlineClass(1, "spring boot", true));
-		springClasses.add(new OnlineClass(2, "spring data jpa", true));
-		springClasses.add(new OnlineClass(3, "spring mvc", false));
-		springClasses.add(new OnlineClass(4, "spring core", false));
 		springClasses.add(new OnlineClass(5, "rest api development", false));
 
-		OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
+		Optional<OnlineClass> spring = springClasses.stream()
+				.filter(oc -> oc.getTitle().startsWith("spring"))
+				.findFirst();
 
+		boolean present = spring.isPresent();
+		System.out.println(present);
 
+		Optional<OnlineClass> jpa = springClasses.stream()
+				.filter(oc -> oc.getTitle().startsWith("jpa"))
+				.findFirst();
+
+		present = jpa.isEmpty();
+		System.out.println(present);
+
+		Optional<OnlineClass> optional = springClasses.stream()
+				.filter(oc -> oc.getTitle().startsWith("spring"))
+				.findFirst();
+
+//		OnlineClass onlineClass = optional.orElseGet(App::createNewClass); //cf. orElse
+//		System.out.println(onlineClass.getTitle());
+
+//		Optional<OnlineClass> onlineClass = optional.filter(oc -> !oc.isClosed());	//filter의 결과가 Optional
+//		System.out.println(onlineClass.isEmpty());
+
+		Optional<Progress> progress = optional.flatMap(OnlineClass::getProgress);
 	}
 
-	
-	
+	private static OnlineClass createNewClass() {
+		System.out.println("creating new online class");
+		return new OnlineClass(10, "New class", false);
+	}
+
+
 }
